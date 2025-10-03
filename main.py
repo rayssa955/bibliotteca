4ebeba173a0eb21779750457da169670488773f6
Rayssa Silva <rayssa.rebeca955@gmail.com>
2025-10-03 13:03:28 -0300

    função de cadastro de livros

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def criando_tabela():
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             titulo TEXT NOT NULL,
             autor TEXT NOT NULL,
-            ano INTEGER
+            ano INTEGER,
             disponivel TEXT          
             )
 """)
@@ -24,4 +24,34 @@ def criando_tabela():
             conexao.close()  
 
 criando_tabela()
-        
+
+#Criando Cadastro
+
+def cadastro_livro(titulo, autor, ano):
+    try:
+        conexao = sqlite3 .connect("biblioteca.db")
+        cursor = conexao.cursor()
+        cursor.execute("""
+            INSERT INTO livros (titulo, autor, ano, disponivel)
+            VALUES (?,?,?,?)                             
+            """,
+            (titulo, autor, ano, "sim")
+        )
+        conexao.commit()
+
+    except Exception as error:
+        #Caso ocorra algum erro no banco
+        print(f"erro ao tentar cadastrar livro {error}")
+    finally:
+        #Sempre fechar a conexão
+        if conexao:
+            conexao.close()  
+           
+
+titulo = input("Digite o titulo do livro desejado: ").lower()
+autor = input("Digite o autor do livro: ").lower()
+ano = int(input("Digite o ano do livro: "))
+
+cadastro_livro(titulo, autor, ano )
+
+
